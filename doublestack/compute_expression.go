6573bd903dca005189c0b17d1aa510896522c0b6
vinefarer <vinefarer@163.com>
2017-10-12 01:09:16 +0800

    commit a test version

--- doublestack/compute_expression.go
+++ doublestack/compute_expression.go
@@ -4,33 +4,55 @@ import (
 	"fmt"
 	"os"
 	"bufio"
-	//"github.com/vinefarer/golist"
+	"github.com/vinefarer/golist"
 )
 
 var (
 	inputReader *bufio.Reader
-	inputValue string
+	inputValue byte
 	err error
 	res interface{}
 )
 
-func evaluate(str string) interface{} {
-	//ops := golist.InitStack()
-	//val := golist.InitStack()
-
-	return str
-}
+//func evaluate(str string) interface{} {
+//	ops := golist.InitStack()
+//	val := golist.InitStack()
+//
+//	return str
+//}
 
 func main() {
 	fmt.Println("Please input your expression : ")
-
 	inputReader = bufio.NewReader(os.Stdin)
-	inputValue, err = inputReader.ReadString('\n')
-	if err != nil {
-		fmt.Println("There were errors reading, exiting program.")
-		return
+	ops := golist.InitStack()
+	val := golist.InitStack()
+	for {
+		inputValue, err = inputReader.ReadByte()
+		if err != nil {
+			fmt.Println("There were errors reading, exiting program.")
+			break
+		}
+		switch inputValue {
+		case '(':
+		case '+': fallthrough
+		case '-': fallthrough
+		case '*': fallthrough
+		case '/': ops.Push(inputValue)
+		case ')': {
+			op := ops.Pop()
+			res := val.Pop()
+			switch op {
+			case '+': res = val.Pop() + res
+			case '-': res = val.Pop() - res
+			case '*': res = val.Pop() * res
+			case '/': res = val.Pop() / res
+			}
+		}
+		default: val.Push(inputValue)
+		}
+		if inputValue == '\n' {
+			break
+		}
+		fmt.Printf("The result is : %s\n", string(inputValue))
 	}
-
-	res = evaluate(inputValue)
-	fmt.Println("The result is : ", res)
 }
